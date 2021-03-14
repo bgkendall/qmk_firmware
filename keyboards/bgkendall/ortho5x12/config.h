748ee69ef4e366bae6a83b35b3961622d6af8a2e
B.G. Kendall <github@bgkendall.com>
2021-03-14 15:52:04 -0400

    Add Ortho5×12 keyboard
    Ortho5×12 is a hand-wired ortholinear keyboard in a steel Preonic shell.
    
    [Build log on Imgur](https://imgur.com/a/7fKZRYG)
    [r/mk post 1]
    
    r/mk Posts:
     * [Hand-wired ortho with hot-swap PCB… Huh?](https://www.reddit.com/r/olkb/comments/kbxpuv/handwired_ortho_with_hotswap_pcb_huh/)
     * [I got some soot on my keyboard during my alchemical experiments](https://www.reddit.com/r/MechanicalKeyboards/comments/kfx5s8/i_got_some_soot_on_my_keyboard_during_my/)
     * [Let’s trash my layout…](https://www.reddit.com/r/olkb/comments/kp75tg/lets_trash_my_layout/)
     * [Just started trying out a 30% layout on a 50% board, so *on average* it is a 40…](https://www.reddit.com/r/MechanicalKeyboards/comments/lglvpy/just_started_trying_out_a_30_layout_on_a_50_board/)
     * [Alchemy-appropriate encoder knob finally arrived](https://www.reddit.com/r/MechanicalKeyboards/comments/ly3em8/alchemyappropriate_encoder_knob_finally_arrived/)

---
+++ keyboards/bgkendall/ortho5x12/config.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x424B  // "BK"
+#define PRODUCT_ID      0x005C  // "5.12"
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    bgkendall
+#define PRODUCT         Ortho5x12
+#define DESCRIPTION     B.G. Kendall Ortho 5×12
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 12
+
+/* Bootmagic reset key */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 11
+
+/* key matrix pins                                                             Pro Micro pins */
+#define MATRIX_ROW_PINS { D2, D7, B3, C6, E6 }                              // RX1, 6, 14, 5, 7
+#define MATRIX_COL_PINS { D3, B5, B6, B2, F7, B1, F4, D1, F5, D0, F6, D4 }  // TX0, 9, 10, 16, A0, 15, A3, 2, A2, 3, A1, 4
+#define UNUSED_PINS
+
+/* encoder pins and settings */
+#define ENCODERS_PAD_A { D5 }                                               // TxLED
+#define ENCODERS_PAD_B { B0 }                                               // RxLED
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCING_DELAY 5
+
+/* Use right-hand modifiers for LM() */
+#define LAYER_MOD_USE_RIGHT
+
+/* prevent stuck modifiers */
+#define PREVENT_STUCK_MODIFIERS
+
+/* Number of taps before a tap-toggle layer locks */
+#define TAPPING_TOGGLE 2
+
+#define RGB_DI_PIN B4                                                       // 8
+#ifdef RGB_DI_PIN
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+/* #define RGBLIGHT_INDICATOR 5,1  // 1 x 3U */
+#define RGBLIGHT_HUE_STEP 2
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 15
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_LAYER_BLINK
+#endif
---
+++ keyboards/bgkendall/ortho5x12/keymaps/3x12/keymap_extra.c
@@ -0,0 +1,14 @@
+#include "bgkendall/include/encoder.h"
+
+const uint16_t PROGMEM encodermaps[][1][2] =
+{
+    [KL_DEF]         = { { KC_PGDN, KC_PGUP } },
+    [KL_DEF1]        = { { G(KC_MINS), G(KC_EQL) } },
+    [KL_DEF2]        = { { KC_LBRC, KC_RBRC } },
+    [KL_DEF3]        = { { KC_WH_U, KC_WH_D } },
+    [KL_DEF4]        = { { KC_WH_R, KC_WH_L } },
+    [KL_NUM]         = { { KC_VOLD, KC_VOLU } },
+    [KL_NP]          = { { KC_PGDN, KC_PGUP } },
+    [KL_FN...KL_FN1] = { { KC_BRID, KC_BRIU } },
+    [KL_META]        = { { RGB_VAD, RGB_VAI } }
+};
