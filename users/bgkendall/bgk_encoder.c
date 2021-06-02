0d9994fa82bb7bf5881c9fd21f533758efd0ddae
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2021-06-02 01:28:19 -0400

    Update encoder library
    Based on keyboards/nightly_boards/\*/encoder_action.\* by Neil Brian Ramirez, new method for setting encoder actions. This method requires that the actions are defined in the keymap. Switch Ortho5x12 to use the handwired/5x13 layout, with the first column being a dummy that will set the encoder actions.
    
    (WIP) Also started working on an alternate version that will support keymaps for existing keyboards that do not have extra places in the layout to add the encoder keys (e.g., the Gherkin). This will use an additional layer that will define all the encoder actions for the other layers based on the position.

--- users/bgkendall/bgk_encoder.c
+++ users/bgkendall/bgk_encoder.c
@@ -1,20 +1,113 @@
-//
-//  bgk_encoder.c
-//  qmk-keyboard
-//
-//  Created by Barnaby on 2021-04-19.
-//  © 2020 Barnaby Kendall. All rights reserved.
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * "ENCODERS_CLOCKWISE_KEY" logic based on
+ * keyboards/nightly_boards/.../encoder_action.h/c by Neil Brian Ramirez.
+ */
 
 #include "bgk_encoder.h"
+
+
+#if defined(ENCODERS_CLOCKWISE_KEY) || defined(ENCODERS_KEY_LAYER)
+
+static uint8_t encoder_state[BGK_NUM_ENCODERS] = {0};
+
+
+#ifdef ENCODERS_CLOCKWISE_KEY
+
+static keypos_t encoder_keys[2][BGK_NUM_ENCODERS] = { ENCODERS_ANTI_CLOCKWISE_KEY, ENCODERS_CLOCKWISE_KEY };
+
+keypos_t get_key_position(uint8_t index, bool clockwise)
+{
+    return encoder_keys[(clockwise ? 1 : 0)][index];
+}
+
+#endif // ENCODERS_CLOCKWISE_KEY
+
+#ifdef ENCODERS_KEY_LAYER
+
+static keypos_t encoder_layers[BGK_NUM_ENCODERS] = { ENCODERS_KEY_LAYER };
+// Allow setting of which keys are mapped for each layer **IF** this is needed (test by messing with mapping)
+
+keypos_t get_key_position(uint8_t index, bool clockwise)
+{
+    set_oneshot_layer(encoder_layers[index], ONESHOT_START);
+
+    return (keypos_t)
+    {
+        (clockwise ? 0 : 1),
+        get_highest_layer(layer_state)
+    };
+}
+
+#endif // ENCODERS_KEY_LAYER
+
+void encoder_action_register(uint8_t index, bool clockwise)
+{
+    keyevent_t encoder_event =
+        (keyevent_t)
+        {
+            .key = get_key_position(index, clockwise),
+            .pressed = true,
+            .time = (timer_read() | 1)
+        };
+    encoder_state[index] = (clockwise ^ 1) | (clockwise << 1);
+
+    action_exec(encoder_event);
+}
+
+void encoder_action_unregister(void)
+{
+    for (int index = 0; index < BGK_NUM_ENCODERS; index++)
+    {
+        if (encoder_state[index])
+        {
+            keyevent_t encoder_event =
+                (keyevent_t)
+                {
+                    .key = get_key_position(index, (encoder_state[index] >> 1)),
+                    .pressed = false,
+                    .time = (timer_read() | 1)
+                };
+            encoder_state[index] = 0;
+            action_exec(encoder_event);
+        }
+    }
+}
+
+void matrix_scan_kb(void)
+{
+    encoder_action_unregister();
+    matrix_scan_user();
+}
+
+void encoder_update_kb(uint8_t index, bool clockwise)
+{
+    encoder_action_register(index, clockwise);
+};
+
+
+#else  // ! ENCODERS_CLOCKWISE_KEY || ENCODERS_KEY_LAYER
+
 
 #include "mousekey.h"
 
 
-bool (*bgkencoder_custom_keycode_handler)(uint16_t,bool) = NULL;
+bool (*bgkencoder_custom_keycode_handler)(uint16_t, bool) = NULL;
 
 
-void bgkencoder_init(bool (*custom_keycode_handler)(uint16_t,bool))
+void bgkencoder_init(bool (*custom_keycode_handler)(uint16_t, bool))
 {
     bgkencoder_custom_keycode_handler = custom_keycode_handler;
 }
@@ -68,7 +161,10 @@ void bgkencoder_update(uint8_t encoder_index, bool clockwise)
     }
 }
 
-void encoder_update_user(uint8_t encoder_index, bool clockwise)
+void encoder_update_kb(uint8_t encoder_index, bool clockwise)
 {
     bgkencoder_update(encoder_index, clockwise);
 }
+
+
+#endif // ENCODERS_CLOCKWISE_KEY || ENCODERS_KEY_LAYER
--- users/bgkendall/bgk_encoder.h
+++ users/bgkendall/bgk_encoder.h
@@ -1,10 +1,18 @@
-//
-//  bgk_encoder.h
-//  qmk-keyboard
-//
-//  Created by Barnaby on 2020-10-10.
-//  © 2020 Barnaby Kendall. All rights reserved.
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #pragma once
 
@@ -13,12 +21,11 @@
 #include "quantum.h"
 
 static pin_t bgkencoders_pad_a[] = ENCODERS_PAD_A;
-extern const uint16_t encodermaps[][sizeof(bgkencoders_pad_a)/sizeof(pin_t)][2];
+#define BGK_NUM_ENCODERS sizeof(bgkencoders_pad_a)/sizeof(pin_t)
+extern const uint16_t encodermaps[][BGK_NUM_ENCODERS][2];
 
-void bgkencoder_init(bool (*custom_keycode_handler)(uint16_t,bool));
+void bgkencoder_init(bool (*custom_keycode_handler)(uint16_t, bool));
 
 void bgkencoder_update(uint8_t encoder_index, bool clockwise);
 
-void encoder_update_user(uint8_t encoder_index, bool clockwise);
-
-#endif /* encoder_enable */
+#endif // ENCODER_ENABLE
