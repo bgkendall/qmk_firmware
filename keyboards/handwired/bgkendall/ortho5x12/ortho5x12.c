8577e8660ca4f8baabccd55222898186372dfa3c
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2021-06-14 23:05:10 -0400

    Update Ortho 5×12 custom key handling
     * Allow CK_COMDOT to repeat. Note that.
        * only Left Shift will switch Comma to Dot;
        * Right Shift will trigger a Less-Than; and
        * Left and Right Shift together will trigger a Greater-Than.
     * Left Control with Grave-Delete will trigger an Escape (at least until I get a more reliable rotary encoder…).

--- keyboards/handwired/bgkendall/ortho5x12/ortho5x12.c
+++ keyboards/handwired/bgkendall/ortho5x12/ortho5x12.c
@@ -94,6 +94,8 @@ layer_state_t layer_state_set_kb(layer_state_t state)
     return state;
 }
 
+static bool is_left_shift_pressed = false;
+static bool comma_dot_was_shifted = false;
 static bool grave_del_was_shifted = false;
 static bool paren_was_shifted = false;
 
@@ -106,123 +108,104 @@ bool process_record_kb(uint16_t keycode, keyrecord_t* record)
     {
         switch (keycode)
         {
-        case CK_M1:
-            SEND_STRING(TEXT_STRING_1 "\n");
-            process = false;
-            break;
-        case CK_M2:
-            SEND_STRING(TEXT_STRING_2 "\n");
-            process = false;
-            break;
-        case CK_M3:
-            if (modifiers & MOD_BIT(KC_LGUI))
+            case CK_M1:
+                SEND_STRING(TEXT_STRING_1 "\n");
+                process = false;
+                break;
+            case CK_M2:
+                SEND_STRING(TEXT_STRING_2 "\n");
+                process = false;
+                break;
+            case CK_M3:
+                if (modifiers & MOD_BIT(KC_LGUI))
+                {
+                    unregister_code(KC_LGUI);
+                    SEND_STRING(TEXT_STRING_3g);
+                    register_code(KC_LGUI);
+                }
+                else if (modifiers & MOD_BIT(KC_RGUI))
+                {
+                    unregister_code(KC_RGUI);
+                    SEND_STRING(TEXT_STRING_3g);
+                    register_code(KC_RGUI);
+                }
+                else if (modifiers & MOD_BIT(KC_LALT))
+                {
+                    unregister_code(KC_LALT);
+                    SEND_STRING(TEXT_STRING_3a);
+                    register_code(KC_LALT);
+                }
+                else if (modifiers & MOD_BIT(KC_RALT))
+                {
+                    unregister_code(KC_RALT);
+                    SEND_STRING(TEXT_STRING_3a);
+                    register_code(KC_RALT);
+                }
+                else if (modifiers & MOD_BIT(KC_LCTL))
+                {
+                    unregister_code(KC_LCTL);
+                    SEND_STRING(TEXT_STRING_3c);
+                    register_code(KC_LCTL);
+                }
+                else if (modifiers & MOD_BIT(KC_RCTL))
+                {
+                    unregister_code(KC_RCTL);
+                    SEND_STRING(TEXT_STRING_3c);
+                    register_code(KC_RCTL);
+                }
+                else
+                {
+                    SEND_STRING(TEXT_STRING_3);
+                }
+                process = false;
+                break;
+            case CK_MULT:
+                tap_code16(C(KC_SPACE));  // Switch to Unicode input (hopefully)
+                register_code(KC_RALT);   // Hold down right alt
+                SEND_STRING("00d7");      // Send Unicode for multiplication sign
+                unregister_code(KC_RALT); // Release right alt
+                tap_code16(C(KC_SPACE));  // Switch away from Unicode input
+                process = false;
+                break;
+            case CK_THOU:
+                tap_code16(KC_P0);
+                tap_code16(KC_P0);
+                tap_code16(KC_P0);
+                process = false;
+                break;
+            case CK_TONL:
             {
-                unregister_code(KC_LGUI);
-                SEND_STRING(TEXT_STRING_3g);
-                register_code(KC_LGUI);
+                uint8_t new_layer = get_highest_layer(layer_state) + 1;
+                if (new_layer > KL_DEF4) { new_layer = KL_DEF; }
+                layer_move(new_layer);
+                process = false;
+                break;
             }
-            else if (modifiers & MOD_BIT(KC_RGUI))
-            {
-                unregister_code(KC_RGUI);
-                SEND_STRING(TEXT_STRING_3g);
-                register_code(KC_RGUI);
-            }
-            else if (modifiers & MOD_BIT(KC_LALT))
-            {
-                unregister_code(KC_LALT);
-                SEND_STRING(TEXT_STRING_3a);
-                register_code(KC_LALT);
-            }
-            else if (modifiers & MOD_BIT(KC_RALT))
-            {
-                unregister_code(KC_RALT);
-                SEND_STRING(TEXT_STRING_3a);
-                register_code(KC_RALT);
-            }
-            else if (modifiers & MOD_BIT(KC_LCTL))
-            {
-                unregister_code(KC_LCTL);
-                SEND_STRING(TEXT_STRING_3c);
-                register_code(KC_LCTL);
-            }
-            else if (modifiers & MOD_BIT(KC_RCTL))
-            {
-                unregister_code(KC_RCTL);
-                SEND_STRING(TEXT_STRING_3c);
-                register_code(KC_RCTL);
-            }
-            else
-            {
-                SEND_STRING(TEXT_STRING_3);
-            }
-            process = false;
-            break;
-        case CK_COMDOT:
-            if (modifiers & MOD_BIT(KC_LSHIFT))
-            {
-                unregister_code(KC_LSHIFT);
-                tap_code16(KC_DOT);
-                register_code(KC_LSHIFT);
-            }
-            else if (modifiers & MOD_BIT(KC_RSHIFT))
-            {
-                unregister_code(KC_RSHIFT);
-                tap_code16(KC_DOT);
-                register_code(KC_RSHIFT);
-            }
-            else
-            {
-                tap_code16(KC_COMM);
-            }
-            process = false;
-            break;
-        case CK_MULT:
-            tap_code16(C(KC_SPACE));  // Switch to Unicode input (hopefully)
-            register_code(KC_RALT);   // Hold down right alt
-            SEND_STRING("00d7");      // Send Unicode for multiplication sign
-            unregister_code(KC_RALT); // Release right alt
-            tap_code16(C(KC_SPACE));  // Switch away from Unicode input
-            process = false;
-            break;
-        case CK_THOU:
-            tap_code16(KC_P0);
-            tap_code16(KC_P0);
-            tap_code16(KC_P0);
-            process = false;
-            break;
-        case CK_TONL:
-        {
-            uint8_t new_layer = get_highest_layer(layer_state) + 1;
-            if (new_layer > KL_DEF4) { new_layer = KL_DEF; }
-            layer_move(new_layer);
-            process = false;
-            break;
-        }
-        case KC_TAB:
-            if (get_highest_layer(layer_state) >= KL_FN &&
-                get_highest_layer(layer_state) <= KL_FN2)
-            {
-                bgkey_register_command_for_tab();
-            }
-            break;
-        case RGB_TGVAD:
-            rgblight_decrease_val();
-            if (rgblight_get_val() == 0)
-            {
-                rgblight_disable();
-            }
-            process = false;
-            break;
-        case RGB_TGVAI:
-            if (!rgblight_is_enabled())
-            {
-                rgblight_enable();
-            }
-            rgblight_increase_val();
-            process = false;
-            break;
-        default:
-            break;
+            case KC_TAB:
+                if (get_highest_layer(layer_state) >= KL_FN &&
+                    get_highest_layer(layer_state) <= KL_FN2)
+                {
+                    bgkey_register_command_for_tab();
+                }
+                break;
+            case RGB_TGVAD:
+                rgblight_decrease_val();
+                if (rgblight_get_val() == 0)
+                {
+                    rgblight_disable();
+                }
+                process = false;
+                break;
+            case RGB_TGVAI:
+                if (!rgblight_is_enabled())
+                {
+                    rgblight_enable();
+                }
+                rgblight_increase_val();
+                process = false;
+                break;
+            default:
+                break;
         }
     }
 
@@ -230,34 +213,70 @@ bool process_record_kb(uint16_t keycode, keyrecord_t* record)
     {
         switch (keycode)
         {
-        case CK_GDEL:
-            if (record->event.pressed)
-            {
-                grave_del_was_shifted = modifiers & MOD_MASK_SG;
-                add_key(grave_del_was_shifted ? KC_GRAVE : KC_DELETE);
-            }
-            else
-            {
-                del_key(grave_del_was_shifted ? KC_GRAVE : KC_DELETE);
-            }
-            send_keyboard_report();
-            process = false;
-            break;
-        case CK_PAREN:
-            if (record->event.pressed)
-            {
-                paren_was_shifted  = modifiers & MOD_MASK_SHIFT;
-                add_key(paren_was_shifted ? KC_RIGHT_PAREN : KC_LEFT_PAREN);
-            }
-            else
-            {
-                del_key(paren_was_shifted ? KC_RIGHT_PAREN : KC_LEFT_PAREN);
-            }
-            send_keyboard_report();
-            process = false;
-            break;
-        default:
-            break;
+            case KC_LSFT:
+                is_left_shift_pressed = record->event.pressed;
+                break;
+            case CK_COMDOT:
+                if (record->event.pressed)
+                {
+                    comma_dot_was_shifted = is_left_shift_pressed;
+                    if (is_left_shift_pressed)
+                    {
+                        unregister_code(KC_LSHIFT);
+                    }
+                    add_key(comma_dot_was_shifted ? KC_DOT : KC_COMM);
+                }
+                else
+                {
+                    del_key(comma_dot_was_shifted ? KC_DOT : KC_COMM);
+                    if (is_left_shift_pressed)
+                    {
+                        register_code(KC_LSHIFT);
+                    }
+                }
+                send_keyboard_report();
+                process = false;
+                break;
+            case CK_GDEL:
+                if (record->event.pressed)
+                {
+                    if (modifiers & MOD_BIT(KC_LCTL))
+                    {
+                        unregister_code(KC_LCTL);
+                        tap_code16(KC_ESCAPE);
+                        register_code(KC_LCTL);
+                    }
+                    else
+                    {
+                        grave_del_was_shifted = modifiers & MOD_MASK_SG;
+                        add_key(grave_del_was_shifted ? KC_GRAVE : KC_DELETE);
+                    }
+                }
+                else
+                {
+                    if (!(modifiers & MOD_BIT(KC_LCTL)))
+                    {
+                        del_key(grave_del_was_shifted ? KC_GRAVE : KC_DELETE);
+                    }
+                }
+                send_keyboard_report();
+                process = false;
+                break;
+            case CK_PAREN:
+                if (record->event.pressed)
+                {
+                    paren_was_shifted  = modifiers & MOD_MASK_SHIFT;
+                    add_key(paren_was_shifted ? KC_RIGHT_PAREN : KC_LEFT_PAREN);
+                }
+                else
+                {
+                    del_key(paren_was_shifted ? KC_RIGHT_PAREN : KC_LEFT_PAREN);
+                }
+                send_keyboard_report();
+                process = false;
+                break;
+            default:
+                break;
         }
     }
 
