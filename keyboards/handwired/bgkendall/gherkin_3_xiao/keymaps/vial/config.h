854477d67cdb1728a817c2660b82d4a022cd567b
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-08 00:42:41 -0400

    Add Gherkin+3 variants

---
+++ keyboards/handwired/bgkendall/gherkin_3_xiao/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 bgkendall (@bgkendall)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x23, 0x36, 0x18, 0x8B, 0x34, 0x31, 0x47, 0x45}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 } // Top left and right corners
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
---
+++ keyboards/handwired/bgkendall/gherkin_3_xiao/rules.mk
@@ -0,0 +1,8 @@
+
+WS2812_DRIVER = vendor
+
+# For Vial version of QMK:
+BOOTLOADER = rp2040
+BACKLIGHT_DRIVER = software
+
+USER_NAME := bgkendall
