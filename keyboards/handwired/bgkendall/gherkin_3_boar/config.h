67cdfdf75aca83dfa1e51c7e64d120cfcc79bf6e
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-09 03:09:11 -0400

    Update Gherkin+3 (Ɓ︎) for Vial
     * Update Vial keymap to match hardware
     * Add dummy row to match Gherkin+3 (小) for 5-way encoder (and also RGB layer indicator key)
     * Initialise encoder map
     * Other minor tweaks

--- keyboards/handwired/bgkendall/gherkin_3_boar/config.h
+++ keyboards/handwired/bgkendall/gherkin_3_boar/config.h
@@ -8,7 +8,7 @@
 #define RGBLIGHT_POWER_PIN  GP28
 
 // Set layer indicator key to key that is unused because of nav. switch
-#define RGBLIGHT_LAYER_KEY  {3,0}
+#define RGBLIGHT_LAYER_KEY  {3,6}
 
 // For Vial version of QMK:
 #define BACKLIGHT_LEVELS 5
