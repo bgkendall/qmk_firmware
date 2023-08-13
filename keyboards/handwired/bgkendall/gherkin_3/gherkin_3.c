fad49ccc61db3d92333b473f448481cf452f7f77
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-12 22:39:54 -0400

    Add support for WS2812 status indicator (phase 1)
    The status indicator has been added as a WS2812 LED using the existing driver code, but appearing as a custom driver for the RGB matrix.

--- keyboards/handwired/bgkendall/gherkin_3/gherkin_3.c
+++ keyboards/handwired/bgkendall/gherkin_3/gherkin_3.c
@@ -39,18 +39,14 @@ bool caps_word_press_user(uint16_t keycode)
 #endif // CAPS_WORD_ENABLE
 
 
-/****************************************************************************
+/*****************************************************************************
  * RGB                                                                       *
  *****************************************************************************/
 
-#ifdef RGBLIGHT_ENABLE
-
-#include "users/bgkendall/bgk_rgb.h"
-
-
-enum RGB_LAYERS
+enum RGBS
 {
-    RGBL_A = 0,
+    RGBL_OFF = 0,
+    RGBL_A,
     RGBL_B,
     RGBL_C,
     RGBL_D,
@@ -72,10 +68,97 @@ enum RGB_LAYERS
     RGBL_CAPW,
     RGBL_CAPL,
     RGBL_OK
+ };
+
+uint8_t get_rgb_layer(layer_state_t state)
+{
+
+#ifdef RGBLIGHT_LAYER_KEY
+    uint16_t rgb_layer_key = keymap_key_to_keycode(get_highest_layer(state),
+                                                   (keypos_t)RGBLIGHT_LAYER_KEY);
+printf("State: %i", state);
+printf(", key: %i\n", rgb_layer_key);
+
+    if (rgb_layer_key >= KC_A && rgb_layer_key <= KC_S)
+    {
+        return (uint8_t)(rgb_layer_key - KC_A + 1);
+    }
+#endif // RGBLIGHT_LAYER_KEY
+
+    return 0;
+}
+
+
+#ifdef RGB_MATRIX_ENABLE
+
+#include "color.h"
+#include "users/bgkendall/bgk_rgb.h"
+
+const HSV bgk_rgb_layers[] = {
+    [RGBL_OFF] = { HSV_BLACK },
+
+    [RGBL_F] = { HSV_WHITE },
+    [RGBL_R] = { HSV_RED },
+    [RGBL_K] = { HSV_CORAL },
+    [RGBL_J] = { HSV_ORANGERED },
+    [RGBL_O] = { HSV_ORANGE },
+    [RGBL_N] = { HSV_GOLDENROD },
+    [RGBL_D] = { HSV_GOLD },
+    [RGBL_L] = { HSV_YELLOW },
+    [RGBL_H] = { HSV_CHARTREUSE },
+    [RGBL_G] = { HSV_GREEN },
+    [RGBL_S] = { HSV_SPRINGGREEN },
+    [RGBL_Q] = { HSV_TURQUOISE },
+    [RGBL_E] = { HSV_TEAL },
+    [RGBL_C] = { HSV_CYAN },
+    [RGBL_A] = { HSV_AZURE },
+    [RGBL_B] = { HSV_BLUE },
+    [RGBL_I] = { HSV_PURPLE },
+    [RGBL_M] = { HSV_MAGENTA },
+    [RGBL_P] = { HSV_PINK },
+
+    [RGBL_CAPW] = { HSV_VIVIDPINK },
+    [RGBL_CAPL] = { HSV_RED },
+    [RGBL_OK]   = { HSV_GREEN }
 };
 
-const rgblight_segment_t* const PROGMEM bgk_rgb_layers[] = RGBLIGHT_LAYERS_LIST
+bool rgb_matrix_indicators_kb(void)
+{
+    if (!rgb_matrix_indicators_user())
+    {
+        return false;
+    }
+
+    uint8_t rgb_layer = RGBL_OFF;
+
+    if (host_keyboard_led_state().caps_lock)
+    {
+        rgb_layer = RGBL_CAPL;
+    }
+    else if (is_caps_word_on())
+    {
+        rgb_layer = RGBL_CAPW;
+    }
+    else
+    {
+        rgb_layer = get_rgb_layer(layer_state);
+    }
+
+    rgb_matrix_sethsv_noeeprom(bgk_rgb_layers[rgb_layer].h,
+                               bgk_rgb_layers[rgb_layer].s,
+                               bgk_rgb_layers[rgb_layer].v);
+
+    return true;
+}
+
+#elifdef RGBLIGHT_ENABLE
+
+#include "users/bgkendall/bgk_rgb.h"
+
+const rgblight_segment_t* const PROGMEM bgk_rgb_layers[] = RGBLIGHTS_LIST
 (
+    [RGBL_OFF] = bgkrgb_black_layer,
+
     [RGBL_F] = bgkrgb_white_layer,
     [RGBL_R] = bgkrgb_red_layer,
     [RGBL_K] = bgkrgb_coral_layer,
@@ -101,10 +184,6 @@ const rgblight_segment_t* const PROGMEM bgk_rgb_layers[] = RGBLIGHT_LAYERS_LIST
     [RGBL_OK]   = bgkrgb_green_layer
 );
 
-uint8_t get_rgb_layer(layer_state_t state)
-{
-    return (uint8_t)(keymap_key_to_keycode(get_highest_layer(state), (keypos_t)RGBLIGHT_LAYER_KEY) - KC_A);
-}
 
 layer_state_t layer_state_set_kb(layer_state_t state)
 {
@@ -125,7 +204,7 @@ void caps_word_set_user(bool active)
     rgblight_set_layer_state(RGBL_CAPW, active);
 }
 
-#endif // RGBLIGHT_ENABLE
+#endif // RGB_MATRIX_ENABLE / RGBLIGHT_ENABLE
 
 
 void keyboard_post_init_kb(void)
@@ -134,8 +213,20 @@ void keyboard_post_init_kb(void)
     // Enable/disable debugging:
     debug_enable = true;
     debug_matrix = false;
+    debug_keyboard = false;
+    debug_mouse = false;
 #endif
 
+#ifdef RGB_MATRIX_ENABLE
+
+# ifdef RGBLIGHT_POWER_PIN
+    setPinOutput(RGBLIGHT_POWER_PIN);
+    writePinHigh(RGBLIGHT_POWER_PIN);
+    // TODO: Only enable the pin when the light is needed?
+# endif
+
+    rgb_matrix_enable_noeeprom();
+#endif
 
 #ifdef RGBLIGHT_ENABLE
 
--- users/bgkendall/rules.mk
+++ users/bgkendall/rules.mk
@@ -8,4 +8,13 @@ ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
     SRC += bgk_rgb.c
 endif
 
+ifeq ($(strip $(RGB_STATUS_WS2812_ENABLE)), yes)
+
+    SRC += bgk_status_ws2812.c
+    WS2812_DRIVER_REQUIRED = yes
+	CFLAGS += -DRGB_STATUS_WS2812_ENABLE
+
+endif
+
+
 SRC += bgk_keycommands.c bgk_keycodes.c
