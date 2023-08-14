fdcddea99c1a08fb725e8f0e06c5e0b0e61064fb
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-13 23:57:17 -0400

    Remove unused RGB light and light layer code

--- keyboards/handwired/bgkendall/gherkin_3/gherkin_3.c
+++ keyboards/handwired/bgkendall/gherkin_3/gherkin_3.c
@@ -194,61 +194,6 @@ bool rgb_matrix_indicators_kb(void)
 
 #endif // RGB_MATRIX_ENABLE
 
-#if defined(RGBLIGHT_ENABLE) && defined(RGBLIGHT_LAYERS)
-
-#include "users/bgkendall/bgk_rgb.h"
-
-const rgblight_segment_t* const PROGMEM bgk_rgb_layers[] = RGBLIGHT_LAYERS_LIST
-(
-    [RGBL_OFF] = bgkrgb_black_layer,
-
-    [RGBL_F] = bgkrgb_white_layer,
-    [RGBL_R] = bgkrgb_red_layer,
-    [RGBL_K] = bgkrgb_coral_layer,
-    [RGBL_J] = bgkrgb_orangered_layer,
-    [RGBL_O] = bgkrgb_orange_layer,
-    [RGBL_N] = bgkrgb_goldenrod_layer,
-    [RGBL_D] = bgkrgb_gold_layer,
-    [RGBL_L] = bgkrgb_yellow_layer,
-    [RGBL_H] = bgkrgb_chartreuse_layer,
-    [RGBL_G] = bgkrgb_green_layer,
-    [RGBL_S] = bgkrgb_springgreen_layer,
-    [RGBL_Q] = bgkrgb_turquoise_layer,
-    [RGBL_E] = bgkrgb_teal_layer,
-    [RGBL_C] = bgkrgb_cyan_layer,
-    [RGBL_A] = bgkrgb_azure_layer,
-    [RGBL_B] = bgkrgb_blue_layer,
-    [RGBL_I] = bgkrgb_purple_layer,
-    [RGBL_M] = bgkrgb_magenta_layer,
-    [RGBL_P] = bgkrgb_pink_layer,
-
-    [RGBL_CAPW] = bgkrgb_vividpink_layer,
-    [RGBL_CAPL] = bgkrgb_red_layer,
-    [RGBL_OK]   = bgkrgb_green_layer
-);
-
-
-layer_state_t layer_state_set_kb(layer_state_t state)
-{
-    bgkrgb_set_all_layers(get_rgb_layer(state), RGBL_A, RGBL_S);
-
-    return layer_state_set_user(state);
-}
-
-bool led_update_kb(led_t led_state)
-{
-    rgblight_set_layer_state(RGBL_CAPL, led_state.caps_lock);
-
-    return true;
-}
-
-void caps_word_set_user(bool active)
-{
-    rgblight_set_layer_state(RGBL_CAPW, active);
-}
-
-#endif // RGBLIGHT_ENABLE + RGBLIGHT_LAYERS
-
 
 enum VIAL_JSON_CUSTOM_KEYCODES
 {
@@ -328,24 +273,5 @@ void keyboard_post_init_kb(void)
 # ifdef WS2812_POWER_PIN
     setPinOutput(WS2812_POWER_PIN);
     writePinHigh(WS2812_POWER_PIN);
-    // TODO: Only enable the pin when the light is needed?
 # endif
-
-#ifdef RGBLIGHT_ENABLE
-
-
-    // Turn off lighting:
-    rgblight_disable();
-
-# ifdef RGBLIGHT_LAYERS
-    // Enable the LED layers:
-    rgblight_layers = bgk_rgb_layers;
-
-    // Flash OK layer:
-    rgblight_blink_layer_repeat(RGBL_OK, 333, 3);
-
-# endif
-
-#endif // RGBLIGHT_ENABLE
-
 }
