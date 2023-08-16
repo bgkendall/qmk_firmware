5ca4e7e4d4e1a4d2a1195d1aba5a40761bee3faf
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-16 01:23:24 -0400

    Add ProMicro driven Gherkin+3
    It all fits (just), even running Vial!

---
+++ keyboards/handwired/bgkendall/gherkin_3/keymaps/micro/rules.mk
@@ -0,0 +1,19 @@
+ifneq (,$(findstring vial,$(CURDIR)))
+
+    $(info Enabling Vial)
+
+    # Current directory contains "vial", so enable Vial Settings
+    VIA_ENABLE = yes
+    VIAL_ENABLE = yes
+    VIAL_INSECURE = yes
+
+    # Save space
+    GRAVE_ESC_ENABLE = no
+    QMK_SETTINGS = no
+    CONSOLE_ENABLE = no
+
+else
+
+    COMBO_ENABLE = no
+
+endif
