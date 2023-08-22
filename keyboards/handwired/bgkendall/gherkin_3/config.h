99dc334ddccfc35f83cf96e07a0b390d20b7c94d
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2023-08-21 20:53:21 -0400

    Add default Tap-Hold settings
    This is primarily for the ProMicro board that does not have access to QMK Settings in Vial due to space.

--- keyboards/handwired/bgkendall/gherkin_3/config.h
+++ keyboards/handwired/bgkendall/gherkin_3/config.h
@@ -6,6 +6,10 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
+// Default Tap-Hold settings
+#define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
+
 // #ifdef NO_DEBUG
 // #   undef NO_DEBUG
 // #endif
