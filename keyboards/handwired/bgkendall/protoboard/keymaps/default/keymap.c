e63ac8e8e8ba239e721c2118ac964619d4dc67ed
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-05-19 17:54:12 -0400

    Tweak prototyping boards

--- keyboards/handwired/bgkendall/protoboard/keymaps/default/keymap.c
+++ keyboards/handwired/bgkendall/protoboard/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
     [KL_DEFAULT ... KL_DEFAULT_COPY] = LAYOUT
     (
-        KC_1,               KC_2,
+        RGB_TOG,            RGB_MOD,
         KC_3,               KC_4,
         KC_5,               KC_6,
         KC_X
