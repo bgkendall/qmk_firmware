7ae402919829517d747168d1864911ade32699fe
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-05-19 17:54:12 -0400

    Tweak prototyping boards

--- keyboards/handwired/bgkendall/protoboard2/config.h
+++ keyboards/handwired/bgkendall/protoboard2/config.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#include "config_common.h"
-
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
--- keyboards/handwired/bgkendall/protoboard2/protoboard2.c
+++ keyboards/handwired/bgkendall/protoboard2/protoboard2.c
@@ -1,1 +1,7 @@
-#include "protoboard2.h"
+#include QMK_KEYBOARD_H
+
+void keyboard_post_init_kb(void)
+{
+    setPinOutput(GP25);
+    writePinHigh(GP25);
+}
